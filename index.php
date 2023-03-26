8f87acf550f920935896bdee2fe47a00ce511952
aldoalfiansyah <56811994+aldoalfiansyah@users.noreply.github.com>
2023-03-26 09:18:37 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Fynzz-kunn</title>
+  <style type="text/css">
+    body {font-family: Calibri, Helvetica, Arial, sans-serif;}
+    h3 {font-family: Cambria,"Times New Roman",serif;}
+    #paragraf2 {font-family: Georgia, serif;}
+    </style>
+  <style>
+    /*untuk teks heading ditag<body>*/
+    h3
+    {
+      font: sans-serif;
+      margin: auto;
+      padding: 15px;
+    }
+
+    body
+    {
+      background-image: url('images/bg.jpg');
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
+
+    p
+    {
+      font-family: Georgia;
+      font-size: 15px;
+      margin: auto;
+      padding: 15px;
+    }
+ 
+    </style>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm navbar-dark"style="background:black">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="#">
+    <img src="https://i.pinimg.com/564x/66/9b/29/669b29c13978f49e2dc633816f588a6a.jpg" alt="" style="width:45px;">
+  </a>
+  
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="https://www.tiktok.com/@fynzz.w6?_t=8axk8JcMz33&_r=1"><img src="https://i.pinimg.com/564x/18/32/cc/1832ccdc1c3cf65ac53c07d18aca616c.jpg" style="width:35px;"></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="https://saweria.co/fynzz"><img src="https://i.pinimg.com/564x/d7/23/66/d72366e4111bab13b420f384f0ae7972.jpg" alt="" style="width:35px;"></a>
+    </li>
+  </ul>
+</nav>
+
+<div class="container-fluid">
+  <h3><strong>Hai Guys</strong></h3>
+  <p>Preset ada di Youtube ya guys, Dan kalo kalian pengen cepet dapet info tentang preset
+    <br>bisa join grup Telegram yahh :)  </p>
+    <button class="yotube"><a href="https://youtube.com/@aldoyt1797?si=EnSIkalECMiOmarE">My Youtube</a></button>
+    <button class="telegram"><a href="https://t.me/+Qm274dMdadxlYjY1">Grup Telegram</a></button>
+    </div>
+
+</body>
+</html>
